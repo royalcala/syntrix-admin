f048feef03680e402a8ad665634b59e5b84d11dc
Roy Alcala Ortiz <alcala.rao@gmail.com>
2026-06-18 21:44:01 +0000

    Add vite-plugin-react-click-to-component

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+import { clickToComponent } from "vite-plugin-react-click-to-component";
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), clickToComponent()],
   clearScreen: false,
   server: {
     port: 1421,
